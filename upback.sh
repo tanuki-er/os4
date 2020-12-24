2bfd88a2c10d3a32ec100afd394958e9c9808cb1
Mika <62598350+tanuki-er@users.noreply.github.com>
2020-12-24 12:15:05 +0300

    Update upback.sh

--- upback.sh
+++ upback.sh
@@ -4,15 +4,18 @@ from=/home/user
 to=/home/user/restore/
 
 maxDate="1970-01-01"
-for file in $(ls $from | grep "Backup-"); do
-  date=$(echo $file | sed "s/Backup-//")
-  if [[ $date > $maxDate ]]; then
-    maxDate=$date
-  fi
+for file in $(ls $from | grep "Backup-")
+do
+date=$(echo $file | sed "s/Backup-//")
+if [[ $date > $maxDate ]]
+then
+maxDate=$date
+fi
 done
-
-if [[ maxDate != "1970-01-01" ]]; then
-  for file in $(ls "$from/Backup-$maxDate" | grep -v -E "*.[0-9]{4}-[0-9]{2}-[0-9]{2}$"); do
-    cp "$from/Backup-$maxDate/$file" "$to"
-  done
+if [[ maxDate != "1970-01-01" ]]
+then
+for file in $(ls "$from/Backup-$maxDate" | grep -v -E "*.[0-9]{4}-[0-9]{2}-[0-9]{2}$")
+do
+cp "$from/Backup-$maxDate/$file" "$to"
+done
 fi
